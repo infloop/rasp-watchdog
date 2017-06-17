f4952b8e883c9f3bac1fc3cd154cb69ec5b2ba6f
infloop <infloop.gm@gmail.com>
2017-06-17 22:25:19 +0300

    Test #2

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-var gpio = require('pi-gpio');
+var gpio = require('rpi-gpio');
 
-gpio.open(7, 'output', function(err) {		// Open pin 16 for output
-    gpio.write(7, 1, function() {			// Set pin 16 high (1)
-        gpio.close(7);						// Close pin 16
-    });
-});
+var pin   = 7;
+var delay = 2000;
+var count = 0;
+var max   = 3;
+
+gpio.setup(pin, gpio.DIR_OUT, on);
+
+function on() {
+    if (count >= max) {
+        gpio.destroy(function() {
+            console.log('Closed pins, now exit');
+        });
+        return;
+    }
+
+    setTimeout(function() {
+        gpio.write(pin, 1, off);
+        count += 1;
+    }, delay);
+}
+
+function off() {
+    setTimeout(function() {
+        gpio.write(pin, 0, on);
+    }, delay);
+}
